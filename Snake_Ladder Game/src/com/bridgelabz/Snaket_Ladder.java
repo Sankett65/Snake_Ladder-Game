23284bd8430f6f2560903e5dbd81da8530e2b4ce
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-09-05 20:24:35 +0530

    Winning Target is 100

--- Snake_Ladder Game/src/com/bridgelabz/Snaket_Ladder.java
+++ Snake_Ladder Game/src/com/bridgelabz/Snaket_Ladder.java
@@ -4,52 +4,64 @@ import java.util.Random;
 
 public class Snaket_Ladder {
     public static int user1=0;
+    public static int target =100;
+
+    public static int count=0;
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Snaket & Ladder Game");
 
         System.out.println("!!! The player will start from 0 position !!!");
-        int target =100;
-        while(user1!=target) {
+
+
+      while (user1<=target){
+          if (user1 == target) {
+              break;
+          }
+
             Random r = new Random();
-          
             int deice = r.nextInt(1, 7);
+            count++;
             System.out.println("\nRolling the Deice and you got: " + deice);
 
+          int check = r.nextInt(3);
 
-            int check = r.nextInt(3);
-            if (check == 0) {
-                if (user1 == target) {
-                    break;
-                }
+            if (check == 1) {
                 System.out.println("You have got Ladder");
                 user1 = user1 + deice;
+                if (user1>target){
+                    user1=user1 - deice;
+                    System.out.println("Current position of user is: " + user1);
+                    break;
+                }
                 System.out.println("Current position of user is: " + user1);
-            } else if (check == 1) {
+            } else if (check == 2) {
                 if (user1 == target) {
                     break;
                 }
                 System.out.println("You have got Snake");
                 user1 = user1 - deice;
+                if (user1 > target) {
+                    user1 = user1;
+                    break;
+                }
                 if (user1 > 0) {
                     user1 = user1;
                     System.out.println("Current position of user is: " + user1);
                 } else {
-                    if (user1 == target) {
-                        break;
-                    }
-                    user1 = user1 + deice;
                     System.out.println("Current position of user is: " + user1);
-
                 }
-            } else {
+            }else {
                 System.out.println("You have got Nothing");
-                user1 = user1 + deice;
+                user1 = user1 ;
                 System.out.println("Current position of user is: " + user1);
 
             }
         }
-
+      if (user1==target){
+          System.out.println("\n!!! You Have Won !!!");
+      }
 
     }
 }
